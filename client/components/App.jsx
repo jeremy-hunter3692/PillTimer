8c0c717abc183452bf94615003aaba1a7a4b69e8
paul-luo-22 <108055124+paul-luo-22@users.noreply.github.com>
2022-08-25 17:35:08 +1200

    Create blank canvas

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
+import Pixel from './Pixel'
+
 const App = () => {
-  return <div>React development has begun!</div>
+  let array = []
+  for (let i = 0; i < 4000; i++) {
+    array.push(<Pixel id={i} key={i} />)
+  }
+  return (
+    <>
+      <h1>Testing!</h1>
+      <div className="canvas">{array}</div>
+    </>
+  )
 }
 
 export default App
+
+//4000 , 80, 50
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+{
+  /* <div class="grid">
+<div class="column"> </div>
+<div class="column"> </div>
+<div class="column"> </div>
+<div class="column"> </div>
+</div> */
+}
+
+function Pixel() {
+  let style = {
+    width: '5px',
+    height: '5px',
+    backgroundColor: 'white',
+  }
+  return <div style={style}></div>
+}
+
+export default Pixel
