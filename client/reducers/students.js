590592c752c17673144bd321ca85f336501f954b
Jeremy-Hunter <19340214+jeremy-hunter3692@users.noreply.github.com>
2022-11-04 10:41:05 +1300

    students bug fixed events doubling not fixed

--- client/reducers/students.js
+++ client/reducers/students.js
@@ -6,7 +6,7 @@ export default function students(state = empty, action) {
   const { type, payload } = action
   switch (type) {
     case SET_STUDENTS_DATA:
-      return [...state, ...payload]
+      return payload
     default:
       return state
   }
