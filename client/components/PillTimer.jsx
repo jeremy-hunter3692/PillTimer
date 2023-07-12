f7dd09b9467c82d2a8d1c4f0e784fdf0f0cf0a59
Jeremy-Hunter <19340214+jeremy-hunter3692@users.noreply.github.com>
2023-07-12 16:23:27 +1200

    working delete li item delete with small bug

--- client/components/PillTimer.jsx
+++ client/components/PillTimer.jsx
@@ -2,16 +2,37 @@ import React, { useState } from 'react'
 import moment from 'moment'
 
 export default function PillTimer() {
-
   const [tramArr, setTramArr] = useState([])
   const [alreadyAdded, setAdded] = useState(null)
- 
+
+  function deleteButton(x) {
+    //return button with an in built call to delete function passing in the time to find list item in stateful array
+    return (
+      <button
+        onClick={(e) => {
+          e.preventDefault()
+          handleDelete(x)
+        }}
+      >
+        delete
+      </button>
+    )
+  }
+
+  function handleDelete(time) {
+    setTramArr((x) => {
+      return x.filter((x) => x !== time)
+    })
+  }
+
   function timeNow() {
     let nowString = moment().calendar()
     if (nowString != tramArr[tramArr.length - 1]) {
       setTramArr([...tramArr, nowString])
     } else {
       console.log('added')
+
+      //bug when deleted and then added again. CHeck somethign here
       setAdded(nowString)
     }
   }
@@ -28,7 +49,16 @@ export default function PillTimer() {
         Tramadol
       </button>
       <h1>You took tramadol at </h1>
-      <ul>{tramArr[0] ? tramArr.map((x) => <li key={x}>{x}</li>) : ''}</ul>
+      <ul>
+        {tramArr[0]
+          ? tramArr.map((x) => (
+              <li key={x}>
+                {x}
+                {deleteButton(x)}
+              </li>
+            ))
+          : ''}
+      </ul>
       {alreadyAdded ? <h3> {alreadyAdded} already saved</h3> : ''}
     </>
   )
